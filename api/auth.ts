a884d9f6e896db9eb189159948b45e3912f89e5a
Sachin <sachin@Sachins-MacBook-Air.local>
2026-08-15 09:17:56 +0530

    MoveToNextJs

---
+++ api/auth.ts
@@ -0,0 +1,18 @@
+/**
+ * API service for Authentication
+ */
+
+export const login = async (credentials: any) => {
+  // TODO: Implement login logic
+  console.log("Login with", credentials);
+};
+
+export const register = async (userData: any) => {
+  // TODO: Implement register logic
+  console.log("Register with", userData);
+};
+
+export const refreshToken = async () => {
+  // TODO: Implement refresh token logic
+  console.log("Refreshing token...");
+};
